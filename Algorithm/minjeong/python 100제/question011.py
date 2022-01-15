d758bb7da49ec65a60a452c467426ad9bd823d5d
Minjeong Bak <doniminp@gmail.com>
2022-01-15 22:52:07 +0900

    Add: python 100제 문제11 ~ 문제13

---
+++ Algorithm/minjeong/python 100제/question011.py
@@ -0,0 +1,19 @@
+# 문제11 : for를 이용한 기본 활용
+
+# 1부터 100까지 모두 더하는 Code를  완성하세요. for를 사용해야 합니다.
+
+s = 0
+
+for num in range(1, 101):
+    s += num
+
+print(s)
+
+# 답안
+
+s = 0
+
+for i in range(101):
+		s += i
+
+print(s)
