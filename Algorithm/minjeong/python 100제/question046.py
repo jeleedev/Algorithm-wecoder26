a7b8726d3881f308f53c63531fe258d3510dec58
Minjeong Bak <doniminp@gmail.com>
2022-02-14 20:58:31 +0900

    Add: python 100제 문제46 ~ 48

---
+++ Algorithm/minjeong/python 100제/question046.py
@@ -0,0 +1,26 @@
+# 문제46 : str 자료형의 응용
+
+# 1부터 100까지의(100을 포함) 모든 숫자를 일렬로 놓고 모든 자릿수의 총 합을 구하세요. 
+
+# 예를 들어 10부터 15까지의 모든 숫자를 일렬로 놓으면 101112131415이고
+# 각 자리의 숫자를 더하면 25입니다.
+
+num = [str(i) for i in range(1, 101)]
+add = 0
+
+for j in range(len(num)):
+    for n in range(len(num[j])):
+        add += int(num[j][n])
+
+print(add)
+
+# 답안
+
+s = ''
+for i in range(101):
+    s += str(i)
+print(s)
+result = 0
+for i in s:
+    result += int(i)
+print(result)
